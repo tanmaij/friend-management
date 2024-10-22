bffc4bb834cd0d7a9235bb72837b2068f4f110ef
tanmaij <tanmaijnguyen@gmail.com>
2024-10-22 13:24:32 +0700

    fix: migration init db file

--- data/migrations/000001_init_db.down.sql
+++ data/migrations/000001_init_db.down.sql
@@ -1,1 +1,7 @@
 DROP TABLE IF EXISTS "relationships";
+
+DROP TYPE IF EXISTS "relationship_type";
+
+DROP INDEX IF EXISTS "email_users_idx";
+
+DROP TABLE IF EXISTS "users";
--- data/migrations/000001_init_db.up.sql
+++ data/migrations/000001_init_db.up.sql
@@ -1,13 +1,25 @@
+--create users table
+CREATE TABLE "users" (
+  "id" SERIAL PRIMARY KEY,
+  "email" TEXT NOT NULL,
+  "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS "email_users_idx" ON "users"("email");
+
 CREATE TYPE "relationship_type" AS ENUM ('friend', 'subscribe', 'block');
 
 --create relationships table
 CREATE TABLE "relationships" (
-   "id" SERIAL PRIMARY KEY,
-   "requester_email" VARCHAR NOT NULL,
-   "target_email" VARCHAR NOT NULL,
-   "type" relationship_type NOT NULL,
-   "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
-   "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+  "id" SERIAL PRIMARY KEY,
+  "requester_id" INT NOT NULL,
+  "target_id" INT NOT NULL,
+  "type" relationship_type NOT NULL,
+  "created_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+  FOREIGN KEY ("requester_id") REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY ("target_id") REFERENCES "users" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-CREATE UNIQUE INDEX "requester_email_target_email_type_relationships_idx" ON "relationships"("requester_email", "target_email","type");
+CREATE UNIQUE INDEX IF NOT EXISTS "requester_id_target_id_relationships_idx" ON "relationships"("requester_id", "target_id","type");
